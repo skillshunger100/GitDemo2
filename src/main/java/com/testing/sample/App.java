8171a325e29199525f359549f61f4f26ace1bdfb
skillshunger100 <skillshunger47@gmail.com>
2023-08-16 18:19:25 +0530

    American guy commited

--- src/main/java/com/testing/sample/App.java
+++ src/main/java/com/testing/sample/App.java
@@ -11,5 +11,10 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("I'm GitHub");
         System.out.println("1st commit");
+        
+        /////////
+        System.out.println( "Hello India" );
+        System.out.println("I'm GitHub pro");
+       
     }
 }
